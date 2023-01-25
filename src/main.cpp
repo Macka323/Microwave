b6f86ab9081108c94134552919d8f1ad3cefe86a
Martin Milchevski <75259519+Macka323@users.noreply.github.com>
2023-01-26 00:05:47 +0100

    *

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <Arduino.h>
-#include <BluetoothSerial.h>
-BluetoothSerial BtSerial;
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char *ssid = "dd-wrt";
+const char *password = "modecom32";
+// Domain name with URL path or IP address with path
+String serverName = "http://192.168.10.12:1880/update-sensor";
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
 TaskHandle_t Task1;
 
 #define RED_TEMP_SENSOR_PIN 14
@@ -19,39 +27,56 @@ void SetHeater(bool state)
   digitalWrite(HEATER_PIN_2, !state);
 }
 
+bool GetDor()
+{
+  return digitalRead(DOR_BUTTON_PIN);
+}
+
+bool GetHeater()
+{
+  return digitalRead(HEATER_PIN_1) && digitalRead(HEATER_PIN_2);
+}
+
 void SetFan(bool state)
 {
   digitalWrite(FAN_PIN, !state);
 }
 
-int GetTemp(){
+int GetTemp()
+{
   int red = analogRead(RED_TEMP_SENSOR_PIN);
   int black = analogRead(RED_TEMP_SENSOR_PIN);
-  int average = (red+black)/2;
+  int average = (red + black) / 2;
 
-  //convert from sensor readings to temperature
-  float temp=(average-684.98)/22.07;
+  // convert from sensor readings to temperature
+  float temp = (average - 684.98) / 22.07;
 
   return temp;
 }
 
-void SetTargetTemp(int temp){
-  targetTemp=temp;
+void SetTargetTemp(int temp)
+{
+  targetTemp = temp;
 }
 
 void BtSerialCode(void *parameter)
 {
-  for (;;)
-  {
-    if (BtSerial.available() > 0)
-    {
-      BtSerial.println(BtSerial.read());
-    }
-  }
 }
 
 void setup()
 {
+
+  pinMode(RED_TEMP_SENSOR_PIN, INPUT);
+  pinMode(BLACK_TEMP_SENSOR_PIN, INPUT);
+  pinMode(DOR_BUTTON_PIN, INPUT);
+
+  pinMode(HEATER_PIN_1, OUTPUT);
+  pinMode(HEATER_PIN_2, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
+
+  SetFan(false);
+  SetHeater(false);
+
   xTaskCreatePinnedToCore(
       BtSerialCode, /* Function to implement the task */
       "Task1",      /* Name of the task */
@@ -60,47 +85,55 @@ void setup()
       0,            /* Priority of the task */
       &Task1,       /* Task handle. */
       0);           /* Core where the atask should run */
-  BtSerial.begin("Microwave v4");
-  Serial.begin(115200);
-  pinMode(RED_TEMP_SENSOR_PIN, INPUT);
-  pinMode(BLACK_TEMP_SENSOR_PIN, INPUT);
-  pinMode(DOR_BUTTON_PIN, INPUT);
 
-  pinMode(HEATER_PIN_1, OUTPUT);
-  pinMode(HEATER_PIN_2, OUTPUT);
-  pinMode(FAN_PIN, OUTPUT);
-  
-  SetFan(false);
-  SetHeater(false);
+  WiFi.begin(ssid, password);
+  Serial.begin(115200);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
-  BtSerial.print("RED: ");
-  BtSerial.print(analogRead(RED_TEMP_SENSOR_PIN));
-  BtSerial.print("                              BLACK: ");
-  BtSerial.println(analogRead(BLACK_TEMP_SENSOR_PIN));
+  if ((millis() - lastTime) > timerDelay)
+  {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      HTTPClient http;
+      String serverPath = serverName + "?temperature=24.37";
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
 
-  BtSerial.print("{");
-  BtSerial.print("\"C0\":\"");//"C0":"
-  BtSerial.print(GetTemp());//xxx.xxx
-  BtSerial.print("\",");//",
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
 
-    BtSerial.print("\"T0\":\"");//"T0":"
-  BtSerial.print(targetTemp);//xxx.xxx
-  BtSerial.print("\",");//",
-
-    BtSerial.print("\"H0\":\"");//"H0":"
-  BtSerial.print(GetTemp());//x
-  BtSerial.print("\",");//",
-
-    BtSerial.print("\"F0\":\"");//"F0":"
-  BtSerial.print(GetTemp());//x
-  BtSerial.print("\",");//",
-
-    BtSerial.print("\"D0\":\"");//"D0":"
-  BtSerial.print(GetTemp());//x
-  BtSerial.print("\",");//",
-  SetFan(true);
-  delay(100);
+      if (httpResponseCode > 0)
+      {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else
+      {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else
+    {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+    SetFan(true);
+    delay(100);
+  }
 }
